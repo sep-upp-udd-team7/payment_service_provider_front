a0910d80c51bb369baaba4aed3cf7e397da85e90
sanjadrinic <sanja.drinic@uns.ac.rs>
2022-12-21 18:09:14 +0100

    feat: added outcome of bank payment pages

---
+++ psp-frontend/src/app/components/bank-failed/bank-failed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-failed',
+  templateUrl: './bank-failed.component.html',
+  styleUrls: ['./bank-failed.component.scss']
+})
+export class BankFailedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
